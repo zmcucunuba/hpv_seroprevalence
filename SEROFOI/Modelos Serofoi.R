be86e0d2906aeab9303e514b3c01e2c689169f81
jcdussan1986 <77905630+jcdussan1986@users.noreply.github.com>
2024-01-11 19:34:26 -0500

    foi hpv16 col

--- SEROFOI/Modelos Serofoi.R
+++ SEROFOI/Modelos Serofoi.R
@@ -24,7 +24,7 @@ dat_colp <- dat_col %>%  mutate ( birth_year = tsur - age_mean_f)
 
 dat_col_HPV18p <- dat_col_HPV18 %>% mutate ( birth_year = tsur - age_mean_f)
 
-#FOI constate
+#FOI constate  HPV 18
 HPV_constant <- run_seromodel(serodata = dat_col_HPV18p,
                                foi_model = "constant",
                                n_iters = 1000)
@@ -52,7 +52,7 @@ HPV_normal_log_plot <- plot_seromodel(HPV_normal_log,
 
 cowplot::plot_grid(HPV_constant_plot, HPV_normal_plot , HPV_normal_log_plot,ncol = 3)
 
-## AJUSTE DE LOS MODELOS ### PROBLEMAS EN EL AJUSTE
+## AJUSTE DE LOS MODELOS ### 
 
 HPV_constant <- run_seromodel(serodata = dat_col_HPV18p, foi_model = "constant", n_iters = 1000)
 HPV_normal <- run_seromodel(serodata = dat_col_HPV18p, foi_model = "tv_normal", n_iters = 1500)
@@ -84,3 +84,54 @@ loo1 <- data.frame(loo)
 
 
 openxlsx::write.xlsx(loo1, "LOO_V1.xlsx")
+
+
+# HPV 18 COL
+
+dat_col_HPV16 <- data.frame (filter (dat_col, pathogen == "HPV 16"))
+dat_col_HPV16p <- dat_col_HPV16 %>% mutate ( birth_year = tsur - age_mean_f)
+
+#FOI constate  HPV 18
+HPV_constant16 <- run_seromodel(serodata = dat_col_HPV16p,
+                              foi_model = "constant",
+                              n_iters = 1000)
+
+HPV_constant_plot16 <- plot_seromodel(HPV_constant16, 
+                                    serodata = dat_col_HPV16p, 
+                                    size_text = 12)
+## normal##
+HPV_normal16 <- run_seromodel(serodata = dat_col_HPV16p,
+                            foi_model = "tv_normal",
+                            n_iters = 1500)
+
+HPV_normal_plot16 <- plot_seromodel(HPV_normal16, 
+                                  serodata = dat_col_HPV16p, 
+                                  size_text = 10)
+## normal log##
+
+HPV_normal_log16 <- run_seromodel(serodata = dat_col_HPV16p,
+                                foi_model = "tv_normal_log",
+                                n_iters = 1500)
+
+HPV_normal_log_plot16 <- plot_seromodel(HPV_normal_log16, 
+                                      serodata = dat_col_HPV18p, 
+                                      size_text = 10)
+
+cowplot::plot_grid(HPV_constant_plot16, HPV_normal_plot16 , HPV_normal_log_plot16,ncol = 3)
+
+
+## AJUSTE DE LOS MODELOS VPH16### 
+
+HPV_constant16 <- run_seromodel(serodata = dat_col_HPV16p, foi_model = "constant", n_iters = 1000)
+HPV_normal16 <- run_seromodel(serodata = dat_col_HPV16p, foi_model = "tv_normal", n_iters = 1500)
+HPV_normal_log16 <- run_seromodel(serodata = dat_col_HPV16p, foi_model = "tv_normal_log", n_iters = 1500)
+
+fit2 <- list(constant = HPV_constant, normal = HPV_normal, normal_log = HPV_normal_log)
+waic2 <- mapply(function(z) loo::waic(loo::extract_log_lik(z, parameter_name = "logLikelihood"))$estimates[3, ], fit2)
+waic2 <- as.data.frame(waic2)
+
+
+openxlsx::write.xlsx(waic1, "WAIC_VPH16.xlsx")
+
+
+
