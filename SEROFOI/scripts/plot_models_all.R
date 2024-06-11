a51303d0a38a6a2da6d2de25843e1c62dd529caf
zmcucunuba <zulma.cucunuba@javeriana.edu.co>
2024-06-10 20:09:35 -0500

    plots

--- SEROFOI/scripts/plot_models_all.R
+++ SEROFOI/scripts/plot_models_all.R
@@ -1,37 +1,88 @@
 library(serofoi)
 
+
+fPlotAllModels <- function(res_models, fox_max, foi_max_m4, size_text)
+  
+{
+  survey <- unique(res_models$dat0$survey)
+  
+  S1 <- 
+    plot_seroprevalence_estimates(
+      seromodel = res_models$model1,
+      serosurvey = res_models$dat0,
+      size_text = size_text) +
+    ggtitle(paste0(survey, "/n M1-Constante sin serorevesión"))
+  
+  # No existe por ahora
+  # F1 <- 
+  #   plot_foi_estimates(
+  #   seromodel = res_models$model1,
+  #   serosurvey = res_models$dat0,
+  #   size_text = size_text,
+  #   foi_max = fox_max)
+  
+  
+  S2 <- 
+    plot_seroprevalence_estimates(
+      seromodel = res_models$model2,
+      serosurvey = res_models$dat0,
+      size_text = size_text) +
+    ggtitle(paste0(survey, "/n M2-Tiempo sin seroreversión"))
+  
+  
+  F2 <- plot_foi_estimates(
+    seromodel = res_models$model2,
+    serosurvey = res_models$dat0,
+    size_text = size_text,
+    foi_max = foi_max)
+  
+  
+  S3 <- 
+    plot_seroprevalence_estimates(
+      seromodel = res_models$model3,
+      serosurvey = res_models$res_models,
+      size_text = size_text) +
+    ggtitle(paste0(survey, "/n M3-Edad sin seroreversión"))
+  
+  
+  F3 <- plot_foi_estimates(
+    seromodel = res_models$model3,
+    serosurvey = res_models$dat0,
+    size_text = size_text,
+    foi_max = foi_max)
+  
+  S4 <- 
+    plot_seroprevalence_estimates(
+      seromodel = res_models$model3,
+      serosurvey = res_models$dat0,
+      size_text = size_text) +
+    ggtitle(paste0(survey, "/n M4-Edad con seroreversión"))
+  
+  
+  F4 <- plot_foi_estimates(
+    seromodel = res_models$model3,
+    serosurvey = res_models$dat0,
+    size_text = size_text,
+    foi_max = foi_max_m4)
+  
+  empty_plot <- ggplot() + theme_void()
+  
+  Ss <- cowplot::plot_grid(S1, S2, S3, S4, nrow = 1, align = "hv") 
+  Fs <- cowplot::plot_grid(empty_plot, F2, F3, F4, nrow = 1, align = "hv") 
+  
+  final_plot <- cowplot::plot_grid(Ss, Fs, nrow=2, align = "hv", rel_heights = c(1,1, 0.9))
+  return(final_plot)
+}
+
+
 COL_HPV18 <- readRDS ("SEROFOI/results_RDS/COL-024-02_.RDS")
+plot_COL_HPV18 <- fPlotAllModels(res_models = COL_HPV18, 
+                                 fox_max = 0.1, foi_max_m4 = 1, size_text = 8)
+jpeg(filename = "SEROFOI/plots/COL_HPV18.jpeg",width = 480*3, height = 480)
 
-# MODELO 1
 
+COL_HPV18 <- readRDS ("SEROFOI/results_RDS/COL-024-02_.RDS")
+plot_COL_HPV18 <- fPlotAllModels(res_models = COL_HPV18, 
+                                 fox_max = 0.1, foi_max_m4 = 1, size_text = 8)
+jpeg(filename = "SEROFOI/plots/COL_HPV18.jpeg",width = 480*3, height = 480)
 
-  plot_seroprevalence_estimates(
-  seromodel = COL_HPV18$model1,
-  serosurvey = COL_HPV18$dat0,
-  size_text = 10
-) +
-  ggtitle("M1-Constante sin serorevesión")
- 
- plot_foi_estimates(
-  seromodel = COL_HPV18$model1,
-  serosurvey = COL_HPV18$dat0,
-  size_text = 10,
-  foi_max = 0.04
-)
- 
- plot_seroprevalence_estimates(
-   seromodel = COL_HPV18$model2,
-   serosurvey = COL_HPV18$dat0,
-   size_text = 10
- ) +
-   ggtitle("M2-Tiempo sin seroreversión")
- 
- 
- plot_foi_estimates(
-   seromodel = COL_HPV18$model2,
-   serosurvey = COL_HPV18$dat0,
-   size_text = 10,
-   foi_max = 0.04
- )
-
- cowplot::plot_grid()
